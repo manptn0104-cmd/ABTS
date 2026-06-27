68f0156833585ac9d5b3717d16a182adcf1e3490
Raman Kumar <yraman2103@gmail.com>
2026-06-28 00:47:07 +0530

    Fix API port fallback issues in Rajneesh-dev

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,5 +1,5 @@
-export const API_BASE_URL = 'http://localhost:5000/api';
-export const SOCKET_URL   = 'http://localhost:5000';
+export const API_BASE_URL = 'http://localhost:5001/api';
+export const SOCKET_URL   = 'http://localhost:5001';
 
 export const GOOGLE_MAPS_API_KEY = 'YOUR_GOOGLE_MAPS_API_KEY';
 
